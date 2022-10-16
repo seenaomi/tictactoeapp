4d74f19857ca1034190527bffe9df93889fe8451
Naomi See <naomi.see@mutualofomaha.com>
2022-10-16 12:41:54 -0500

    Adding v 3 of app

--- app/src/main/java/com/capstone/tictactoeapp/MainActivity.java
+++ app/src/main/java/com/capstone/tictactoeapp/MainActivity.java
@@ -1,16 +1,19 @@
 package com.capstone.tictactoeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
 import android.graphics.Point;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private TicTacToe tttGame;
     private Button [][] buttons;
+    private TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         // Create the layout manager as a GridLayout
         GridLayout gridLayout = new GridLayout(this);
         gridLayout.setColumnCount(TicTacToe.SIDE);
-        gridLayout.setRowCount(TicTacToe.SIDE);
+        gridLayout.setRowCount(TicTacToe.SIDE + 1);
 
         // Create the buttons and add them to gridLayout
         buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
@@ -42,6 +45,23 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        // Set up layout parameters of 4th row of gridLayout
+        status = new TextView(this);
+        GridLayout.Spec rowSpec = GridLayout.spec(TicTacToe.SIDE, 1);
+        GridLayout.Spec columnSpec = GridLayout.spec(0, TicTacToe.SIDE);
+        GridLayout.LayoutParams lpStatus = new GridLayout.LayoutParams(rowSpec, columnSpec);
+        status.setLayoutParams(lpStatus);
+
+        //set up status' characteristics
+        status.setWidth(TicTacToe.SIDE * w);
+        status.setHeight(w);
+        status.setGravity(Gravity.CENTER);
+        status.setBackgroundColor(Color.GREEN);
+        status.setTextSize((int) (w * .15));
+        status.setText(tttGame.result());
+
+        gridLayout.addView(status);
+
         // Set gridLayout as the View of this Activity
         setContentView(gridLayout);
     }
@@ -53,7 +73,9 @@ public class MainActivity extends AppCompatActivity {
         } else if(play == 2){
             buttons[row][col].setText("O");
         } if(tttGame.isGameOver()) { // game over, disable buttons
+            status.setBackgroundColor(Color.RED);
             enableButtons(false);
+            status.setText(tttGame.result());
         }
     }
 
@@ -67,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
 
     private class ButtonHandler implements View.OnClickListener{
         public void onClick(View v) {
-            Log.w("MainActivity", "Inside onClick, v = " + v);
             for(int row = 0; row < TicTacToe.SIDE; row++){
                 for(int column = 0; column < TicTacToe.SIDE; column++){
                     if(v == buttons[row][column]){
--- app/src/main/java/com/capstone/tictactoeapp/TicTacToe.java
+++ app/src/main/java/com/capstone/tictactoeapp/TicTacToe.java
@@ -95,4 +95,14 @@ public class TicTacToe {
         }
         turn = 1;
     }
+
+    public String result() {
+        if(whoWon() > 0) {
+            return "Player " + whoWon() + " won!";
+        } else if(canNotPlay()){
+            return "Tie Game!";
+        } else {
+            return "PLAY !!";
+        }
+    }
 }
